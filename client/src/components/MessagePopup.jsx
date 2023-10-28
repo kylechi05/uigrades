e7f958a01799a199ef9792eecae8e621a016c8c1
liaozhu <liao-zhu@uiowa.edu>
2023-10-28 02:19:11 -0500

    darktheme

--- client/src/components/MessagePopup.jsx
+++ client/src/components/MessagePopup.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import {useTheme} from '../context/ThemeContext'
 
 function MessagePopup({message}) {
-  return (
-    <div className='flex justify-center items-center absolute top-16 text-md bg-zinc-700 text-zinc-300 p-4 rounded-2xl'>
-        <h1>{message}</h1>
-    </div>
-  )
+    const {isDarkMode} = useTheme()
+    return (
+        <div className={`flex justify-center items-center absolute top-16 text-md ${isDarkMode ? 'bg-zinc-700 text-zinc-300' : 'bg-zinc-300 text-zinc-700'} p-4 rounded-2xl`}>
+            <h1>{message}</h1>
+        </div>
+    )
 }
 
 export default MessagePopup
