e60037b980039c9e2a0092c95ccfd081dfcad05f
Kyle Chi <kylechi2157@gmail.com>
2024-03-08 22:56:36 -0600

    removed glow from UIGrades

--- client/src/components/LandingNavbar.tsx
+++ client/src/components/LandingNavbar.tsx
@@ -8,7 +8,7 @@ const LandingNavbar = () => {
     return (
       <div className="flex justify-between items-baseline w-full py-10 sticky top-0 left-0 blurred-container z-[50]">
         <div className='flex items-center gap-4 justify-center'>
-          <Link to="/" className='text-zinc-200 text-[1.5rem] cursor-pointer font-bold glow'><span className='text-primary'>UI</span>Grades</Link>
+          <Link to="/" className='text-zinc-200 text-[1.5rem] cursor-pointer font-bold'><span className='text-primary'>UI</span>Grades</Link>
         </div>
         <ul className="flex justify-end items-center gap-5 sm:gap-12 w-full pl-8">
           <Link
