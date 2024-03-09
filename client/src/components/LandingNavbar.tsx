cd48fd69b9b75e3a6d124209ff97db47eac7fba0
Kyle Chi <kylechi2157@gmail.com>
2024-03-08 20:21:49 -0600

    spacing, text placement adjustment

--- client/src/components/LandingNavbar.tsx
+++ client/src/components/LandingNavbar.tsx
@@ -6,11 +6,11 @@ import '../App.css';
 const LandingNavbar = () => {
 
     return (
-      <div className="flex justify-between items-center w-full py-10 sticky top-0 left-0 blurred-container z-[50]">
+      <div className="flex justify-between items-baseline w-full py-10 sticky top-0 left-0 blurred-container z-[50]">
         <div className='flex items-center gap-4 justify-center'>
-          <Link to="/" className='text-zinc-200 text-[1.5rem] cursor-pointer font-bold'><span className='text-primary'>UI</span>Grades</Link>
+          <Link to="/" className='text-zinc-200 text-[1.5rem] cursor-pointer font-bold glow'><span className='text-primary'>UI</span>Grades</Link>
         </div>
-        <ul className="flex justify-end items-center gap-8 w-full">
+        <ul className="flex justify-end items-center gap-5 sm:gap-12 w-full pl-8">
           <Link
             to="/contact"
             className={`text-zinc-400 glow`}
